f01a56173b8451b372d0fdefe7399121ab4c3eb3
brycebien <brycebien@gmail.com>
2023-11-28 20:04:38 -0500

    created prototype GUI for collecting input
    TODO: record inputs to db

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,11 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  var titleController = TextEditingController();
+  var dateController = TextEditingController();
+  var tagsController = TextEditingController();
+  var startTimeController = TextEditingController();
+  var endTimeController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,7 +54,93 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(widget.title),
       ),
-      body: const Text('First Commit'),
+      body: Column(
+        children: [
+          //TASK
+          const Text('Input a task:'),
+          TextField(
+            controller: titleController,
+            decoration: const InputDecoration(hintText: 'Enter a task title'),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+
+          //DATE
+          const Text('Task Date:'),
+          TextField(
+            controller: dateController,
+            onTap: () async {
+              DateTime? selectedDate = await showDatePicker(
+                context: context,
+                firstDate: DateTime(2000),
+                lastDate: DateTime(2100),
+              );
+              if (selectedDate != null) {
+                dateController.text = selectedDate.toLocal().toString();
+              }
+            },
+            decoration: const InputDecoration(hintText: 'Select task date'),
+          ),
+          const SizedBox(height: 16),
+
+          //START TIME
+          const Text('Start Time:'),
+          TextField(
+            controller: startTimeController,
+            onTap: () async {
+              TimeOfDay? selectedTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              if (selectedTime != null) {
+                startTimeController.text = selectedTime.format(context);
+              }
+            },
+            decoration: const InputDecoration(hintText: 'Select start time'),
+          ),
+          const SizedBox(height: 16),
+
+          //END TIME
+          const Text('End Time:'),
+          TextField(
+            controller: endTimeController,
+            onTap: () async {
+              TimeOfDay? selectedTime = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              if (selectedTime != null) {
+                endTimeController.text = selectedTime.format(context);
+              }
+            },
+            decoration: const InputDecoration(hintText: 'Select end time'),
+          ),
+          const SizedBox(height: 16),
+
+          //TAG
+          const Text('Tag:'),
+          TextField(
+            controller: tagsController,
+            decoration:
+                const InputDecoration(hintText: 'Enter tags (comma-separated)'),
+          ),
+          const SizedBox(height: 16),
+
+          //INPUT BUTTON
+          ElevatedButton(
+            onPressed: () {
+              //TODO: input inputs into db
+              print('title: ' + titleController.text);
+              print('date: ' + dateController.text);
+              print('sart time: ' + startTimeController.text);
+              print('end time: ' + endTimeController.text);
+              print('tag: ' + tagsController.text);
+            },
+            child: const Text('Submit'),
+          )
+        ],
+      ),
     );
   }
 }
